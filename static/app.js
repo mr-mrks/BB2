3d81160680566979ad8096ff3917ac08610cb8c0
Markus <81853046+mr-mrks@users.noreply.github.com>
2025-02-01 17:24:42 +0000

    Update app.js

--- static/app.js
+++ static/app.js
@@ -126,4 +126,9 @@ updateForm.addEventListener('submit', (event) => {
     });
 });
 
-function getRandom
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+} 
